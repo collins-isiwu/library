4fcd262b59dbee11a1cd7a71f5ed04f0b1af0585
collinsavor <73594922+collinsavor@users.noreply.github.com>
2022-10-18 23:33:05 +0100

    User registration

--- library/urls.py
+++ library/urls.py
@@ -19,5 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
