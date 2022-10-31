9cb6fa5c428fc371cf129aa4fa11b93659583d2d
collinsavor <73594922+collinsavor@users.noreply.github.com>
2022-10-31 13:40:29 +0100

    permission

--- book/models.py
+++ book/models.py
@@ -1,4 +1,3 @@
-from distutils.command.upload import upload
 import uuid 
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -14,6 +13,11 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cover = models.ImageField(upload_to="covers/", blank=True)
+
+    class Meta:
+        permissions = [
+            ("special_status", "Can read all books"),
+        ]
 
     def __str__(self):
         return self.title
--- book/tests.py
+++ book/tests.py
@@ -1,4 +1,4 @@
-import email
+from django.contrib.auth.models import Permission
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
@@ -12,6 +12,11 @@ class BookTests(TestCase):
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
+        )
+
+        # for permission
+        cls.special_permission = Permission.objects.get(
+             codename = "special_status"
         )
 
         # For Review
@@ -33,13 +38,26 @@ class BookTests(TestCase):
         self.assertEqual(f"{self.book.author}", 'JK Rowling')
         self.assertEqual(f"{self.book.price}", '25.00')
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123")
         response = self.client.get(reverse('book_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Harry Potter')
         self.assertTemplateUsed(response, 'books/book-list.html')
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, "%s?next=/books/" % (reverse("account_login")))
+        response = self.client.get(
+            "%s?next=/books/" % (reverse("account_login")))
+        self.assertContains(response, "Log In")
+
+    def test_book_detail_view_with_permissions(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get("/books/1/")
         self.assertEqual(response.status_code, 200)
