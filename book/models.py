6934da162766970974bc42c6abfa46047a337cb6
collinsavor <73594922+collinsavor@users.noreply.github.com>
2022-10-31 06:32:22 +0100

    file/image upload

--- book/models.py
+++ book/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 import uuid 
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -12,11 +13,10 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
     def __str__(self):
         return self.title
-
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[str(self.id)])
--- library/settings.py
+++ library/settings.py
@@ -163,6 +163,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+# Media Settings for User Uploaded Files/images
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
